3effb41a88ef9e83b7a6d46f45b68c597d48e428
walid <walid.sa3d@gmail.com>
2016-03-25 16:35:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# silex
+![Build](https://travis-ci.org/walidsa3d/silex.svg?branch=master)
+![downloads](https://img.shields.io/pypi/dm/silex.svg)
+![license](https://img.shields.io/pypi/l/silex.svg)
+![version](https://img.shields.io/pypi/v/silex.svg)
+[![PEP8](https://img.shields.io/badge/code%20style-pep8-orange.svg)](https://www.python.org/dev/peps/pep-0008/)
+
+string utilities
+
+## Install (automatic)
+```
+$ pip install silex
+```
+## Install (manual)
+```
+$ git clone 
+$ cd silex
+$ python setup.py install
+```
+## Usage
+```
+```
+### Demo
+
+
+## License
---
+++ silex/silex.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import re
+import unicodedata
+
+
+def decamelize(strinvar):
+    pass
+
+
+def strip_html(stringvar):
+    ''' remove html tags from string '''
+    return re.sub(r'<[^<]+?>', '', stringvar)
+
+
+def unslugify(stringvar):
+    return re.sub(r'-|_', ' ', stringvar)
+
+
+def slugify(stringvar):
+    if isinstance(stringvar, str):
+        stringvar = stringvar.decode('utf-8')
+    slug = unicodedata.normalize('NFKD', stringvar)
+    slug = slug.encode('ascii', 'ignore')
+    slug = slug.strip()
+    slug = slug.lower()
+    slug = unicode(slug)
+    slug = re.sub(r'[^\w\s-]', '', slug)
+    l = re.sub(r' ', '-', slug)
+    return l
+
+
+def chop(stringvar, step):
+    ''' equal-sized chunks '''
+    l = []
+    i = 0
+    while i <= len(stringvar):
+        l.append(stringvar[i:i+step])
+        i = i+step
+    return l
+
+
+def numberex(stringvar):
+    ''' extract numbers from string '''
+    s = re.search('\d+(\.\d*)?', stringvar)
+    if s:
+        return s.group()
+    return
+
+
+def surround(stringvar, prefix, suffix):
+    return ''.join([prefix, stringvar, suffix])
+
+
+def clean(stringvar):
+    ''' squeeze multiple spaces and trim '''
+    s = re.sub('\s+', ' ', stringvar)
+    return s.strip()
+
+
+def kebabcase(stringvar):
+    ''' hello world --> hello-world'''
+    s = stringvar.lower().split()
+    return '-'.join(s)
+
+
+def pascalcase(stringvar):
+    ''' hello world --> HelloWorld'''
+    s = [word.title() for word in stringvar.split()]
+    return ''.join(s)
+
+
+def camelcase(stringvar):
+    ''' hello world --> helloWorld'''
+    s = stringvar.split()
+    s1 = [word.title() for word in s[1:]]
+    s2 = [s[0].lower()]+s1
+    return ''.join(s2)
+
+
+def snakecase(stringvar):
+    ''' hello world --> hello_world'''
+    s = stringvar.lower().split()
+    return '_'.join(s)
+
+print chop("whitespace", 5)
+print numberex('1.2euros')
+print surround('b', '<', '>')
+print strip_html('<p><i>hello</i></p>')
+print unslugify('sex_appeal')
+print slugify(u'je suis\ épuisé')
+print clean('hello\n    word\n')
+print kebabcase('hello  world')
+print pascalcase('hello world')
+print camelcase('hello world')
+print snakecase('hello world')
